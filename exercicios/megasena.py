53a5e998b2bd88446114278d227f87377cb113ca
anaflaviapacova <anaflaviapacova@colegiopaulodetarso.com.br>
2026-02-07 16:54:09 +0000

    .1

---
+++ exercicios/megasena.py
@@ -0,0 +1,9 @@
+import random
+
+def gerar_numeros():
+    numeros = random.sample(range(1, 61), 6)
+    print("Números sorteados da MegaSena:", sorted(numeros))
+
+if __name__ == "__main__":
+    gerar_numeros()
+
---
+++ exercicios/pedra_papel_tesoura.py
@@ -0,0 +1,9 @@
+import random
+
+def jogar():
+    opcoes = ["Pedra", "Papel", "Tesoura"]
+    escolha = random.choice(opcoes)
+    print("O computador escolheu:", escolha)
+
+if __name__ == "__main__":
+    jogar()
